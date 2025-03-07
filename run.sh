d79b04525e6e02ea020c063412967c4573db6c73
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2025-03-06 20:51:54 -0500

    Add: run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mvn clean package
+
+if [ -f /Users/naveenkumar/Tools/apache-jmeter-5.6.3/lib/ext/jmeter-agent-1.0.2-jar-with-dependencies.jar ]; then
+    rm /Users/naveenkumar/Tools/apache-jmeter-5.6.3/lib/ext/jmeter-agent-1.0.2-jar-with-dependencies.jar
+fi
+
+cp target/jmeter-agent-1.0.2-jar-with-dependencies.jar /Users/naveenkumar/Tools/apache-jmeter-5.6.3/lib/ext
