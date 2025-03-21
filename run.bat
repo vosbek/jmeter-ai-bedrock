95510779c800fc4055ebe19064d99e1e7d9c5285
NaveenKumar Namachivayam <catch.nkn@gmail.com>
2025-03-20 21:19:55 -0400

    Bump version

--- run.bat
+++ run.bat
@@ -1,7 +1,6 @@
 @echo off
 call mvn clean package
-if exist C:\Tools\apache-jmeter-5.6.3\lib\ext\jmeter-agent-1.0.4-jar-with-dependencies.jar (
-    del C:\Tools\apache-jmeter-5.6.3\lib\ext\jmeter-agent-1.0.4-jar-with-dependencies.jar
+if exist C:\Tools\apache-jmeter-5.6.3\lib\ext\jmeter-agent-1.0.5-jar-with-dependencies.jar (
+    del C:\Tools\apache-jmeter-5.6.3\lib\ext\jmeter-agent-1.0.5-jar-with-dependencies.jar
 )
-copy target\jmeter-agent-1.0.4-jar-with-dependencies.jar C:\Tools\apache-jmeter-5.6.3\lib\ext
-@REM C:\Tools\apache-jmeter-5.6.3\bin\jmeter -t C:\Tools\apache-jmeter-5.6.3\bin\GreatClips.jmxA
+copy target\jmeter-agent-1.0.5-jar-with-dependencies.jar C:\Tools\apache-jmeter-5.6.3\lib\ext
